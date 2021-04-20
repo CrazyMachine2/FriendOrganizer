a19bbe1de035be1e0c6c327e82af41dad70f04c1
Andrey Andreev <andrey.andreev@quajoo.com>
2021-04-20 11:02:32 +0200

    basic ui build

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Bobe", LastName = "Turboto" };
+            yield return new Friend { FirstName = "Stefko", LastName = "Oboista" };
+            yield return new Friend { FirstName = "Mario", LastName = "Kitarista" };
+            yield return new Friend { FirstName = "Forever", LastName = "Alone" };
+        }
+    }
+}
